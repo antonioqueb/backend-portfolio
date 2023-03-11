f6dae4f49b16c7ca165acd22ca5c20f1fbd93ca0
Antonio Queb <cardstillo@gmail.com>
2023-03-11 11:44:40 -0600

    Fix: admin.py

--- developer_profile/admin.py
+++ developer_profile/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+@admin.register(DeveloperProfile)
+class DeveloperProfileAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+    list_display_links = '__all__'
+    list_filter = '__all__'
+    
