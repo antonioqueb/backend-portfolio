157188aaaf2d8ba5c64082a09858c70dce5c92cb
Antonio Queb <cardstillo@gmail.com>
2023-03-12 11:48:30 -0600

    Add: ssl in the settings

--- backend/settings.py
+++ backend/settings.py
@@ -105,7 +105,11 @@ DATABASES = {
 
     }
 }
-
+  
+  
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+  
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -141,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [    BASE_DIR / "static",]
 MEDIA_URL = '/files/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "files")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
